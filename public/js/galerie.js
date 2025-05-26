b381ad6685a53934bcb68bfa47d23934363d83bb
josefuccef <josefuccef@gmail.com>
2025-05-26 14:05:53 +0100

    updat

--- public/js/galerie.js
+++ public/js/galerie.js
@@ -73,4 +73,15 @@ Swaying photo gallery - hover event @wakana-k - https://codepen.io/wakana-k/pen/
     
     
     
-    
+    if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then(reg => {
+                console.log("Service Worker registered:", reg.scope);
+            })
+            .catch(err => {
+                console.error("Service Worker failed:", err);
+            });
+    });
+}
