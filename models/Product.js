54789d472b02131d8521bb2742da84d8b14483a8
jotv <josefuccef7@gmail.com>
2026-04-02 02:20:05 +0100

    upd 3333

--- models/Product.js
+++ models/Product.js
@@ -12,11 +12,11 @@ const Product = mongoose.model("Product", productSchema);
 
 // 🔹 تحسين البحث على الحقول المهمة
 //Product.collection.createIndex({ LIBELLE: 1 });
-Product.collection.createIndex({ ANPF: 1 });
-Product.collection.createIndex({ GENCOD_P: 1 });
+// Product.collection.createIndex({ ANPF: 1 });
+// Product.collection.createIndex({ GENCOD_P: 1 });
 //Product.collection.createIndex({ FOURNISSEUR_P: 1 }); 
 
 // 🔹 تحسين الترتيب حسب createdAt
-Product.collection.createIndex({ createdAt: -1 });
+// Product.collection.createIndex({ createdAt: -1 });
 
 module.exports = Product;
